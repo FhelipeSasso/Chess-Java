c9be3093a3014249e19a3e6fa9cebfebcd3f0ee7
FhelipeSasso <saselegendas@outlook.com>
2023-12-07 14:49:12 -0300

    class position

---
+++ src/boardGame/Position.java
@@ -0,0 +1,35 @@
+package boardGame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	// builder, getters and setters
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+		
+	}
+}
